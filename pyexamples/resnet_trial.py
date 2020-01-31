ce28bbab576335010c5d0a429ccd7ce40e34faee
joshualin24 <yaoyuyl2@illinois.edu>
2020-01-31 14:11:49 -0600

    next

--- pyexamples/resnet_trial.py
+++ pyexamples/resnet_trial.py
@@ -22,7 +22,9 @@ arch = [
     to_Pool("pool3", offset="(0,0,0)", to="(conv3-east)", height=28, depth=28, width=1),
     to_Conv("conv4", 14, 256, offset="(1,0,0)", to="(pool3-east)", height=32, depth=32, width=8 ),
     to_Pool("pool4", offset="(0,0,0)", to="(conv4-east)", height=28, depth=28, width=1),
-    to_SoftMax("soft1", 10 ,"(3,0,0)", "(pool4-east)", caption="SOFT"  ),
+    to_Conv("conv5", 7, 512, offset="(1,0,0)", to="(pool4-east)", height=32, depth=32, width=16 ),
+    to_Pool("pool5", offset="(0,0,0)", to="(conv5-east)", height=28, depth=28, width=1),
+    to_SoftMax("soft1", 10 ,"(3,0,0)", "(pool5-east)", caption="SOFT"  ),
     #to_connection("pool3", "soft1"),
     to_end()
     ]
