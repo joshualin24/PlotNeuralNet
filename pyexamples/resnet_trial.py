93bdd20f2cfb31aa70a8ff4fb651def6bdeeaaf8
joshualin24 <yaoyuyl2@illinois.edu>
2020-01-31 13:43:56 -0600

    resnet trial

---
+++ pyexamples/resnet_trial.py
@@ -0,0 +1,34 @@
+
+import sys
+sys.path.append('../')
+from pycore.tikzeng import *
+from pycore.blocks  import *
+
+arch = [
+    to_head('..'),
+    to_cor(),
+    to_begin(),
+
+    #input
+    to_input( '../examples/fcn8s/cats.jpg' ),
+
+        to_head( '..' ),
+    to_cor(),
+    to_begin(),
+    to_Conv("conv1", 112, 64, offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=2 ),
+    to_Pool("pool1", offset="(0,0,0)", to="(conv1-east)"),
+    to_Conv("conv2", 56, 64, offset="(1,0,0)", to="(pool1-east)", height=32, depth=32, width=2 ),
+    to_connection( "pool1", "conv2"),
+    to_Pool("pool2", offset="(0,0,0)", to="(conv2-east)", height=28, depth=28, width=1),
+    to_SoftMax("soft1", 10 ,"(3,0,0)", "(pool1-east)", caption="SOFT"  ),
+    to_connection("pool2", "soft1"),
+    to_end()
+    ]
+
+
+def main():
+    namefile = str(sys.argv[0]).split('.')[0]
+    to_generate(arch, namefile + '.tex' )
+
+if __name__ == '__main__':
+    main()
